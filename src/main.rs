1d0019f1b57c307d8ed389584610d13802b5342a
Selman Özleyen <32667648+SelmanOzleyen@users.noreply.github.com>
2020-07-14 13:33:32 +0300

    Created the environment to develop and added a log file to the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use rand;
+
 fn main() {
-    println!("Hello rusty!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    println!("Random : {}",rand::random::<i32>());
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+
+
 }
