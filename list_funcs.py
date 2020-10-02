dce79bce063c4bd7ed41c62e4d8532446eb484b4
SelmanOzleyen <selman.ozleyen2017@gtu.edu.tr>
2020-10-02 09:24:14 +0300

    list_funcs update

--- list_funcs.py
+++ list_funcs.py
@@ -1,5 +1,17 @@
 import re
-reobj = re.compile(r"pub\sfn [^{]+ {", re.MULTILINE)
-with open("./src/server/func_man/functions.rs") as f:
-    for i in re.findall(reobj, f.read()):
+comments = re.compile(r"///(.*)", re.MULTILINE)
+func_params = re.compile(r"fn[^{\"\->\"]+",re.MULTILINE)
+signature = re.compile(r"(?:///.*\s)*\s*pub\sfn\s[^p][^r][^e][^{]+",
+                       re.MULTILINE)
+comment_lines = []
+func_desc = []
+with open("./src/server/func_man/test.rs") as f:
+    for i in re.findall(signature, f.read()):
         print(i)
+        comment_lines = re.findall(comments, i)
+        desc = ""
+        for j in comment_lines:
+            desc += " ".join(j.split())
+            desc += "\n"
+        func_desc += [desc]
+print(func_desc)
