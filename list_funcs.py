67a9d105d006d92373f5d005f08e83bd426dd08a
SelmanOzleyen <selman.ozleyen2017@gtu.edu.tr>
2020-10-07 02:24:10 +0300

    refactor

--- list_funcs.py
+++ list_funcs.py
@@ -2,11 +2,11 @@ import re
 comments = re.compile(r"///(.*)", re.MULTILINE)
 signature = re.compile(r"(?:///.*\s)*\s*pub\sfn\s[^p][^r][^e][^{]+",
                        re.MULTILINE)
-name_n_return = re.compile(r"fn\s+[^{]+",re.MULTILINE)
+name_n_return = re.compile(r"fn\s+[^{]+", re.MULTILINE)
 func_name = re.compile(r"fn\s+([a-z A-Z] [a-z A-Z 0-9 _]*|_ [a-z A-Z 0-9 _]+)")
 comment_lines = []
 func_desc = []
-print("User functions defined in functions are listed below:\n")
+print("User functions defined are listed below:\n")
 with open("./src/server/func_man/functions.rs") as f:
     for i in re.findall(signature, f.read()):
         comment_lines = re.findall(comments, i)
